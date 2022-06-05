afbc43f8085e321da9421210ec998956c4b13652
David Leiße <davidleisse@gmail.com>
2022-06-05 17:22:47 +0200

    First version with GUI

---
+++ decontamination.py
@@ -0,0 +1,134 @@
+##### Version 1.0 ######
+#### David Leiße #######
+##### david.leisse@uni-bielefeld.de #####
+
+from os import listdir
+from os.path import splitext, isfile, join
+import sys
+import json
+
+from zmq import EVENT_CLOSE_FAILED
+
+__usage__ = """
+    python3 decontamination.py
+    --dir <PATH_TO_INPUT_FOLDER>
+"""
+
+
+def get_files(directory: str) -> list:
+    """
+    Getting all .ali and .log files from the input directory.
+
+    :param String directory: Path to input directory.
+    :return ali: list with all paths to .ali files
+    :return log: list with all paths to .log files
+    """
+
+    onlyfiles = [join(directory, f) for f in listdir(directory) if isfile(join(directory, f))]
+    ali = [f for f in onlyfiles if splitext(f)[-1] == ".ali"]
+    ali.sort()
+    log = [f for f in onlyfiles if splitext(f)[-1] == ".log"]
+    log.sort()
+
+    return ali, log
+
+def sanity_check(ali,log):
+    """
+    Checking if there are the same number of .ali files and .log files.
+
+    :param List ali: list with all paths to .ali files
+    :param List log: list with all paths to .log files
+    """
+    
+    if not len(ali) == len(log):
+        sys.exit("There are not an equal number of .ali files and .log files!!")
+
+def parse_files(file) -> dict:
+    """
+    Parsing .ali file and putting IDs corresponding to their sequence into a dictionary
+
+    :param String file: Path to file
+    :return file_dic: dictionary with IDs as key and the corresponding sequence as value.
+    """
+    
+    with open(file, "r") as f:
+        line = f.readline()
+
+        sequence = []
+        header = ""
+        file_dic = {}
+        while line:
+            if line.startswith(">"):
+
+                if sequence and header:
+                    file_dic[header] = sequence
+                
+                sequence = []
+                header = line[1:].strip()
+            
+            else:
+                sequence.append(line)
+
+            line = f.readline()
+
+    return file_dic
+
+def log_task(log, ali_dic):
+    """
+    Reading corresponding .log file and executing tasks: remove_seq
+
+    :param String log: Path to corresponding log file
+    :param Dictionary ali_dic: Dictionary with parsed .ali file
+    :return mod_ali: modified Dictionary
+    """
+    mod_ali_dic = ali_dic.copy()
+
+    task_dic = {}
+    with open(log, "r") as f:
+        line = f.readline()
+        while line:
+            line = line.strip().split(" ")
+            task_dic[" ".join(line[1:3]).replace('"',"")] = line[0]
+            line = f.readline()
+
+    for id,task in task_dic.items():
+        if task == "remove_seq":
+            mod_ali_dic.pop(id)
+    
+    return mod_ali_dic
+
+def write_output(mod_ali_dic, ali_file):
+    """
+    Writing output files from modified dictionary
+
+    :param Dictionary mod_ali_dic: modified Dictionary with contents for decontaminated .ali files
+    :param String ali_file: path to original .ali file
+    """
+    filename = ali_file[:-4] + "_decontaminated.ali"
+    with open(filename, "w") as out:
+        for id,seq in mod_ali_dic.items():
+            out.write(id + "\n")
+            out.write(str(seq) + "\n")
+
+def __Main__(args):
+    if type(args) == str:
+        args = args.strip().split(" ")
+    directory = args[args.index("--dir") +1]
+
+    ali, log = get_files(directory)
+    sanity_check(ali, log)
+    for idx,entry in enumerate(ali):
+        print(idx)
+        ali_dic = parse_files(entry)
+        mod_ali_dic = log_task(log[idx], ali_dic)
+        if not mod_ali_dic == ali_dic:
+            write_output( mod_ali_dic, entry)
+
+if not sys.argv:
+    print("")
+
+elif "--dir" in sys.argv:
+    __Main__(sys.argv)
+
+else:
+    print(__usage__)
---
+++ decontamination_GUI.py
@@ -0,0 +1,36 @@
+##### Version 1.0 ######
+#### David Leiße #######
+##### david.leisse@uni-bielefeld.de #####
+
+import tkinter
+import decontamination as dc
+
+import os
+from tkinter import *
+from tkinter import simpledialog, messagebox
+from tkinter import filedialog
+
+root = Tk()
+root.directory = ""
+
+def loadDirectory():
+    """
+    Defining input directory
+    """
+    initialdir = os.getcwd() 
+    root.directory = filedialog.askdirectory(initialdir = initialdir, title="Select output folder")
+    output.insert(END, "Inputdirectory: " + str(root.directory).split("/")[-1] + "\n")
+
+def Run():
+    dc.__Main__(("--dir " + root.directory))
+    output.insert(END, "Run complete. Check input directory.\n")
+
+root.title("TaxoInflationAnalyzer")
+output = Text(root, width = 90, height = 10, bd = 3, pady=1)
+output.grid(row=0,column=0,columnspan=9)
+Label(root).grid(row=1, columnspan= 9)
+Button(root, text="Input folder",width=10, height=3, command=lambda: loadDirectory()).grid(column=0,row=1)
+Button(root, text="Run", width=10, height=3, command=lambda: Run()).grid(column=1, row=1)
+Button(root, text="Quit",width=10, height=3, command=root.destroy).grid(column=2, row=1)
+
+mainloop()
