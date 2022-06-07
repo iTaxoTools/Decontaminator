4fa92dfcef277cb68ab9b3935a8d61eb388fb918
David Leisse <davidleisse@gmail.com>
2022-06-07 22:10:01 +0200

    shorting sequence names to 85 chars

--- decontamination.py
+++ decontamination.py
@@ -53,6 +53,7 @@ def parse_files(directory, file) -> dict:
     with open(file, "r") as f:
         line = f.readline()
 
+        duplicates = []
         sequence = []
         header = ""
         file_dic = {}
@@ -60,24 +61,27 @@ def parse_files(directory, file) -> dict:
             if line.startswith(">"):
 
                 if sequence and header:
+                    if header in file_dic:
+                        duplicates.append(header)
                     file_dic[header] = sequence
                 
                 sequence = []
-                header = line[1:].strip()
+                header = line[1:86].strip()
             
             else:
                 sequence.append(line)
 
             line = f.readline()
 
-    return file_dic
+    return file_dic, duplicates
 
-def log_task(log, ali_dic):
+def log_task(log, ali_dic, duplicates):
     """
     Reading corresponding .log file and executing tasks: remove_seq
 
     :param String log: Path to corresponding log file
     :param Dictionary ali_dic: Dictionary with parsed .ali file
+    :param List duplicates: List of duplicate sequence names that might have occured by shorting the name
     :return mod_ali: modified Dictionary
     """
     mod_ali_dic = ali_dic.copy()
@@ -90,11 +94,15 @@ def log_task(log, ali_dic):
             task_dic[" ".join(line[1:3]).replace('"',"")] = line[0]
             line = f.readline()
 
+
+    relevant_duplicates = []
     for id,task in task_dic.items():
+        if id in duplicates:
+            relevant_duplicates.append(id)
         if task == "remove_seq":
             mod_ali_dic.pop(id)
     
-    return mod_ali_dic
+    return mod_ali_dic, relevant_duplicates
 
 def write_output(mod_ali_dic, ali_file, outputdirectory):
     """
@@ -121,14 +129,28 @@ def __Main__(args):
     if not isdir(output_directory):
         mkdir(output_directory)
 
+    duplicates = {}
+
     ali, log = get_files(directory)
     sanity_check(ali, log)
     for idx,entry in enumerate(ali):
+        if idx == 4:
+            print(entry)
         print(idx)
-        ali_dic = parse_files(directory, entry)
-        mod_ali_dic = log_task(log[idx], ali_dic)
+        ali_dic, duplicates_ls = parse_files(directory, entry)
+        mod_ali_dic, relevant_duplicates_ls = log_task(log[idx], ali_dic, duplicates_ls)
+        if relevant_duplicates_ls:
+            duplicates[entry] = relevant_duplicates_ls
         if not mod_ali_dic == ali_dic:
             write_output( mod_ali_dic, entry, output_directory)
+
+    if duplicates:
+        print("Some duplicates have occured during the process")
+        print("This has occured due to the shortening of sequence names in log files (max 85 char.)")
+        for file, sequenceids in duplicates.items():
+            print("In file " + file + " these sequence names have duplicates: " + str(sequenceids))
+
+__Main__("--dir Testdaten2")
 
 if not sys.argv:
     print("")
