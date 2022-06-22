3dfd50b2427b7d1f9410836118480c4ba7f58fe2
David Leiße <davidleisse@gmail.com>
2022-06-22 12:23:59 +0200

    Add files via upload

---
+++ Utils.py
@@ -0,0 +1,106 @@
+##### Utils.py #####
+##### David Leiße #####
+##### david.leisse@uni-bielefeld.de #####
+
+import dendropy
+from os import mkdir
+from genericpath import isdir
+
+def load_tree(string, mode, schema) -> dendropy.Tree:
+    """
+    Loading tree from .tre file and plotting it.
+    Working with Newick schema until now.
+
+    :param String string: Relative or absolute path to .tre file or data string
+    :param String mode: Choosing between 'path' and 'string'
+    :param String schema: I.e. 'newick' or 'nexus' depending on .tre file
+
+    :return tree: Dendropy.Tree object
+    """
+    if mode == "path":
+        tree = dendropy.Tree.get(path = string, schema = schema)
+    elif mode == "string":
+        tree = dendropy.Tree.get(data = string, schema = schema)
+        
+    return tree
+
+def load_ali_file(file) -> list[str,str]:
+    """
+    Parsing .ali file and putting IDs corresponding to their sequence into a list
+
+    :param String file: Path to file
+    :return ls: list of lists with each an Sequence ID and the corresponding sequence.
+    """
+
+    with open(file, "r") as f:
+        line = f.readline()
+
+        sequence = []
+        header = ""
+        ls = []
+        while line:
+            if line.startswith(">"):
+
+                if sequence and header:
+                    temp = "".join(sequence).rstrip("\n")
+                    ls.append([header, temp])
+                
+                sequence = []
+                header = line[1:86].strip()
+            
+            else:
+                sequence.append(line)
+
+            line = f.readline()
+
+    return ls
+
+def load_fasta_file(file) -> list[str,str]:
+    """
+    Loading fasta file into list. List containing lists with each an ID and sequence
+
+    :param String file: full path to file
+    :return ls: List containing lists with each an ID and sequence  
+    """
+
+    with open(file, "r") as f:
+        line = f.readline().strip()
+
+        sequence = []
+        header = ""
+        ls = []
+        while line:
+            
+            while line and not line.startswith(">"):
+                sequence.append(line)
+                line = f.readline().strip()
+
+            if sequence and header:
+                ls.append([header, "".join(sequence)])
+                
+            sequence = []
+            header = line[1:] 
+            line = f.readline().strip()
+    
+    return ls
+
+def write_decont_output(directory, file, seq_ls, type):
+    """
+    Writing output files from modified dictionary
+
+    :param List mod_ali_ls: modified list with contents for decontaminated .ali files
+    :param String ali_file: path to original .ali file
+    """
+
+    output_directory = directory + "/decontaminated"
+    if not isdir(output_directory):
+        mkdir(output_directory)
+
+    file = output_directory + "/" + file 
+
+    with open(file, "w") as out:
+        if type == "protein":
+            out.write("#\n#\n")
+        for id,seq in seq_ls:
+            out.write(">" + id + "\n")
+            out.write(seq + "\n")
---
+++ lengthdecont.py
@@ -0,0 +1,90 @@
+### Decontamination Tool 2.0 ###
+### David Leiße ###
+### david.leisse@uni-bielefeld.de###
+
+import sys
+import Utils
+import re
+from os import listdir
+from os.path import isfile, join
+
+
+__Usage__ = """
+            python3 lengthdecont.py
+            --dir <FULL_PATH_TO_INPUT_DIRECTORY>
+            --thresh <FLOAT>
+            --mode <STRING_'percentage'_OR_'absolute'>
+            --type <STRING_'protein'_OR_'nucleotide'>
+
+"""
+
+
+def length_decont(threshhold, seq_ls, mode, type): 
+    """
+    Removes temporaly all noninformation data in a sequence and compares its 
+    length with the length of the original sequence that includes noninformation data.
+    If the length of the decontaminated sequence (in comparison with the original) lies under
+    a set threshhold it gets removed from the sequences list.
+
+    :param Float threshhold: A threshhold set by the user. An absolute or relative number.
+    :param List seq_ls: A list with all sequences from current alignment (.ali) file.
+    :param String mode: String defining if threshhold is a relative or absolute number. ("percentage" or "absolute")
+    :param String type: String defining if sequences are containing aminoacids ("protein") or nucleotides ("nucleotide")
+
+    :return filtered_seq_ls: List containing filtered sequences from alignment (.ali) file.
+    """
+
+    filtered_seq_ls = []
+    popped_seqs = []
+    for i in seq_ls:
+        seq = i[1].lower()
+        if type == "nucleotide":
+            filtered_seq = re.sub('[^atgcryswkm]',"",seq)
+        if type == "protein":
+            filtered_seq = re.sub('[^arndcqeghilkmfpstwyvuo]',"",seq)
+
+        if mode == "percentage":
+
+            perc = len(filtered_seq) / len(seq)
+            if threshhold < perc:
+                filtered_seq_ls.append(i)
+            else:
+                popped_seqs.append(i)
+        
+        elif mode == "absolute":
+
+            if threshhold < len(filtered_seq):
+                filtered_seq_ls.append(i)
+            else:
+                popped_seqs.append(i)
+
+    return filtered_seq_ls
+
+def __Main__(args):
+    directory = args[args.index("--dir") +1]
+    threshhold = float(args[args.index("--thresh") +1])
+    mode = args[args.index("--mode") +1]
+    type = args[args.index("--type") +1]
+
+    files = [f for f in listdir(directory) if isfile(join(directory, f))]
+    
+    for file in files:
+        if type == "protein":
+            seq_ls = Utils.load_ali_file(directory + "/" + file)
+        elif type == "nucleotide":
+            seq_ls = Utils.load_fasta_file(directory + "/" + file)
+        else:
+            print("No correct type argument! Please choose between 'protein' and 'nucleotide'")
+            sys.exit()
+        
+        filtered_seq_ls = length_decont(threshhold, seq_ls, mode, type)
+        Utils.write_decont_output(directory, file, filtered_seq_ls, type)
+
+__Main__(["--dir", "Beispieldateien_-_lengthdecont","--thresh","0.1","--mode","percentage","--type","nucleotide"])
+
+"""
+if "--dir" in sys.argv and "--thresh" in sys.argv and "--mode" in sys.argv and "--type" in sys.argv:
+    __Main__(sys.argv)
+else:
+    print(__Usage__)
+"""
